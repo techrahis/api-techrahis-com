1c04d51e2e1c7794ab7dab49d0015ae19d686621
rajarshisamaddar <itsrajarshi@gmail.com>
2023-08-25 16:54:51 +0530

    update: api route added 💪🏻

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
 const express = require("express");
 var cors = require("cors");
 const app = express();
-app.use(cors());
-const spotify = require("./spotify");
+app.use(cors()); // for CORS poilicy
+app.use(express.urlencoded({ extended: true })); // for accessing res body
+app.set("view engine", "ejs"); // for rendering views
 
+// root api route
 app.get("/", (req, res) => {
-  res.status(200).json({
-    status: 200,
-    author: "rajarshi samaddar",
-    message: "server running 🚀",
-  });
+  res.render("index");
 });
 
-app.get("/now-playing", async (req, res) => {
-  const currentSong = await spotify.currentlyPlayingSong();
-  const song = await currentSong.json();
-  if (song.is_playing) {
-    const data = {
-      id: song.item.id,
-      title: song.item.name,
-      songUrl: song.item.external_urls.spotify,
-      artists: song.item.artists.map((artist) => artist.name).join(", "),
-      is_playing: song.is_playing,
-      album: song.item.album.name,
-      albumImageUrl: song.item.album.images[0].url,
-    };
-    res.status(200).json(data);
-  } else {
-    res.status(200).json({ is_playing: false });
-  }
-});
+// spotify router part
+const spotifyRouter = require("./lib/spotify/router.js");
+app.use("/spotify", spotifyRouter);
+
+// newsletter router part
+const newsletterRouter = require("./lib/newsletter/router.js");
+app.use("/newsletter", newsletterRouter);
 
 app.listen(3300);
