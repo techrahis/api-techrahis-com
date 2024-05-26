245817a470737a7284408134c96dd597d69c51fc
Rajarshi Samaddar <itsrajarshi@gmail.com>
2024-05-26 11:08:11 +0530

    refactor: Remove unused code and files related to movies and resume

--- index.js
+++ index.js
@@ -11,26 +11,14 @@ app.use(express.urlencoded({ extended: true })); // for accessing res body
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "200 OK",
-    route: "api.rajarshisamaddar.com",
+    route: "api.fstackraj.com",
     message: "Server is running at an unstoppable speed 🚀",
   });
 });
 
-// spotify router part
-const spotifyRouter = require("./lib/spotify/router.js");
-app.use("/spotify", spotifyRouter);
-
 // newsletter router part
 const newsletterRouter = require("./lib/newsletter/router.js");
 app.use("/newsletter", newsletterRouter);
-
-// to get the data of recent watched movies and tv series
-const recentMovies = require("./lib/movies/router.js");
-app.use("/movies", recentMovies);
-
-// to get the resume
-const resumeRouter = require("./lib/resume/router.js");
-app.use("/get-resume", resumeRouter);
 
 // to send email notification
 const emailRouter = require("./lib/email/router.js");
